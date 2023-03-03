0b3fe99bc094b1ab36b542bb290b0b9e595ed5e1
Joel-Norman <a.joel015@gmail.com>
2023-03-03 17:20:25 -0300

    update backend amazon

--- src/app/host/host.ts
+++ src/app/host/host.ts
@@ -1,3 +1,3 @@
 //export const host="https://vast-lowlands-38735.herokuapp.com";
-export const host="https://jna-backendservice.onrender.com"
+export const host="Jnaportfolio-env.eba-tg33u3hq.us-east-1.elasticbeanstalk.com"
 //export const host="http://localhost:8080";
